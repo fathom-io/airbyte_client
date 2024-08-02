be4d499a9dad1df6d8c53645a3da68172f3ad883
PiotrGierda <gierda.piotr@gmail.com>
2024-08-02 15:51:43 +0200

    fix: allow for no timestamp in FailureReason models

--- src/models/failure_reason.rs
+++ src/models/failure_reason.rs
@@ -24,11 +24,11 @@ pub struct FailureReason {
     #[serde(rename = "retryable", skip_serializing_if = "Option::is_none")]
     pub retryable: Option<bool>,
     #[serde(rename = "timestamp")]
-    pub timestamp: i64,
+    pub timestamp: Option<i64>,
 }
 
 impl FailureReason {
-    pub fn new(timestamp: i64) -> FailureReason {
+    pub fn new(timestamp: Option<i64>) -> FailureReason {
         FailureReason {
             failure_origin: None,
             failure_type: None,
